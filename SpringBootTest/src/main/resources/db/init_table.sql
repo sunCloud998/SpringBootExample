138e5134d9ccb221661962cda5ddd40b80ba3c13
fayun.sun <fayun.sun@qunar.com>
2017-05-22 10:30:55 +0800

    init-sql

--- SpringBootTest/src/main/resources/db/init_table.sql
+++ SpringBootTest/src/main/resources/db/init_table.sql
@@ -1,1 +1,13 @@
-create table if not exists users (id int not null primary key auto_increment,username varchar(100),password varchar(100),status int);
+-- create table if not exists users (id int not null primary key auto_increment,username varchar(100),password varchar(100),status int);
+
+create TABLE game_play_config
+(
+  id bigserial primary key,
+  game_play_title VARCHAR (256),
+  destination_id text,
+  destination_name text,
+  show_id text,
+  show_name text,
+  status int,
+  create_time TIMESTAMP DEFAULT now()
+);
